3226fca64c1c5ad5390cee429aedbc6ce530e705
nx006 <limazero14@gmail.com>
2023-05-18 01:05:29 +0900

    docs: 주석 생성함
    주석은 chat-gpt의 도움을 받음

--- tpc/tpc.py
+++ tpc/tpc.py
@@ -7,6 +7,22 @@ import requests
 
 
 def fetch_blog_posts_in_period(user_blog_url, start_date, end_date, access_token):
+    """
+    Fetches a list of post URLs from a specified Tistory blog within a certain date range.
+
+    Uses the Tistory API's post list endpoint (https://www.tistory.com/apis/post/list).
+
+    For more detail, read tistory API here: https://tistory.github.io/document-tistory-apis/apis/v1/post/list.html
+
+    Args:
+        user_blog_url (str): The name of the Tistory blog.
+        start_date (datetime.datetime): The start date of the range.
+        end_date (datetime.datetime): The end date of the range.
+        access_token (str): The access token for the Tistory API.
+
+    Returns:
+        list of str: A list of post URLs from the specified blog within the specified date range.
+    """
     posts = []
     page_number = 1
 
@@ -30,6 +46,22 @@ def fetch_blog_posts_in_period(user_blog_url, start_date, end_date, access_token
 
 
 def fetch_blog_posts_in_period_all(user_blog_url_list, start_date, end_date, access_token):
+    """
+    Fetches a list of post URLs from a list of specified Tistory blogs within a certain date range.
+
+    Uses the fetch_blog_posts_in_period function to fetch the posts from each individual blog.
+
+    For more detail, read tistory API here: https://tistory.github.io/document-tistory-apis/apis/v1/post/list.html
+
+    Args:
+        user_blog_url_list (list of str): The list of Tistory blog names.
+        start_date (datetime.datetime): The start date of the range.
+        end_date (datetime.datetime): The end date of the range.
+        access_token (str): The access token for the Tistory API.
+
+    Returns:
+        list of str: A list of post URLs from the specified blogs within the specified date range.
+    """
     all_posts = []
 
     for user_blog_url in user_blog_url_list:
@@ -37,5 +69,3 @@ def fetch_blog_posts_in_period_all(user_blog_url_list, start_date, end_date, acc
         all_posts.extend(user_posts)
 
     return all_posts
-
-
